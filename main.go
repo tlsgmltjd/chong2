05668289317f07e26a2d3a55f7af713669b7833a
신희성 <vvwv3092@gmail.com>
2024-08-25 02:42:32 +0900

    add: 메서드 분리 리펙터링

--- main.go
+++ main.go
@@ -13,12 +13,33 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-func main() {
-
+func loadEnv() {
     err := godotenv.Load()
     if err != nil {
         log.Fatal("env 파일을 찾지 못했어요")
     }
+}
+
+func daily_notic(dg *discordgo.Session) *cron.Cron {
+	c := cron.New()
+	_, err := c.AddFunc("0 8 * * *", func() {
+		channelID := os.Getenv("CHANNEL_ID")
+		_, err := dg.ChannelMessageSend(channelID, "좋은 아침이에요, 오늘 할 일을 공유해주세요!")
+		if err != nil {
+			fmt.Println("메시지 전송 중 에러가 발생했어요: ", err)
+		}
+	})
+
+	if err != nil {
+		log.Fatal("cron 스케줄 등록 중 에러가 발생했어요: ", err)
+	}
+
+	return c
+}
+
+func main() {
+
+	loadEnv()
 
 	token := os.Getenv("DISCORD_BOT_TOKEN")
 
@@ -35,20 +56,8 @@ func main() {
 	}
 	defer dg.Close()
 
-	c := cron.New()
-	_, err = c.AddFunc("30 2 * * *", func() {
-		channelID := os.Getenv("CHANNEL_ID")
-		_, err := dg.ChannelMessageSend(channelID, "좋은 아침이에요, 오늘 할 일을 공유해주세요!")
-		if err != nil {
-			fmt.Println("메시지 전송 중 에러가 발생했어요: ", err)
-		}
-	})
-
-	if err != nil {
-		log.Fatal("cron 스케쥴 등록중 에러가 발생했어요: ", err)
-	}
-
-	c.Start()
+	dailyCron := daily_notic(dg)
+	dailyCron.Start()
 
 	fmt.Println("봇 활성화 중입니다.")
 	stop := make(chan os.Signal, 1)
@@ -56,5 +65,5 @@ func main() {
 	<-stop
 
 	fmt.Println("봇이 꺼졌습니다.")
-	c.Stop()
+	dailyCron.Stop()
 }
